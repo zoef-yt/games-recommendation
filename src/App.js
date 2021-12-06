f77f4539bb51672993ccb164375a3b91c39e04e9
Zoef <67889207+zoef-yt@users.noreply.github.com>
2021-12-06 21:25:17 +0530

    app.js final changes

--- src/App.js
+++ src/App.js
@@ -57,32 +57,79 @@ export default function App() {
   }
   return (
     <div className="App">
-      <h1> 🎮 Quality Games </h1>
-      <p> Some of my top favorite games. </p>
-      <ul>
-        {categories.map((item, index) => {
-          return (
-            <button onClick={() => categoryClickHandler(item)} key={index}>
-              {item}
-            </button>
-          );
-        })}
-      </ul>{" "}
-      <hr />
-      <h1>{selectedCategory}</h1>
-      <ul>
-        {games[selectedCategory].map((text, index) => {
-          return (
-            <li class={"list-item"} key={index}>
-              {" "}
-              <h3 class={"game-name"}>
-                {text.gameName}{" "}
-                <span class={"rating"}> {text.gameRating} </span>
-              </h3>
-            </li>
-          );
-        })}
-      </ul>
+      <nav class="navigation">
+        <div class="nav-brand">🎮 Quality Games </div>
+        <ul class="list-non-bullet nav-pills list-on-one-line">
+          <li class="list-item-inline">
+            <a class="link" href="https://zoef-shaikh.netlify.app/">
+              Home
+            </a>
+          </li>
+          <li class="list-item-inline">
+            <a
+              class="link"
+              href="https://zoef-shaikh.netlify.app/projects.html"
+            >
+              Projects
+            </a>
+          </li>
+          <li class="list-item-inline">
+            <a class="link" href="https://zoef-shaikh.netlify.app/blog.html">
+              Blog
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="container">
+        <p class="nav-brand"> Some of my top favorite games. </p>
+        <ul>
+          {categories.map((item, index) => {
+            return (
+              <button onClick={() => categoryClickHandler(item)} key={index}>
+                {item}
+              </button>
+            );
+          })}
+        </ul>{" "}
+        <hr />
+        <h1>{selectedCategory}</h1>
+        <ul>
+          {games[selectedCategory].map((text, index) => {
+            return (
+              <li class={"list-item"} key={index}>
+                {" "}
+                <h3 class={"game-name"}>
+                  {text.gameName}{" "}
+                  <span class={"rating"}> {text.gameRating} </span>
+                </h3>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <footer class="footer">
+        <div class="footer-heading">social media presence</div>
+        <ul class="social-links list-non-bullet list-on-one-line">
+          <li class="list-item-inline">
+            <a class="link" href="https://github.com/zoef-yt">
+              Github
+            </a>
+          </li>
+          <li class="list-item-inline">
+            <a class="link" href="https://twitter.com/zoef_yt">
+              Twitter
+            </a>
+          </li>
+          <li class="list-item-inline">
+            <a
+              class="link"
+              href="https://www.linkedin.com/in/zoef-shaikh-279340201/"
+            >
+              Linkedin
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
